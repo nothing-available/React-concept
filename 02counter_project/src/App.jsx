960c9e632ef51a1e23e6e28b0d3aef59a3030d31
Sumit kumar <smtkur31@gmail.com>
2023-09-29 23:41:57 +0530

    useState - previous state value concept

--- 02counter_project/src/App.jsx
+++ 02counter_project/src/App.jsx
@@ -3,33 +3,37 @@ import { useState } from 'react';
 
 function App() {
 
-  let [counter,setCounter] = useState(0);
+	const [counter, setCounter] = useState(0);
 
-  const addvalue = () =>{
+	const addvalue = () => {
 
-	if(counter < 20){
-		setCounter(counter + 1);
+		if (counter < 20) {
+			//Updating state based on the previous state value
+			setCounter(prevCounter => prevCounter + 1); 
+			setCounter(prevCounter => prevCounter + 1);
+			setCounter(prevCounter => prevCounter + 1);
+
+		}
 	}
-  }
 
-  const removeValue = () =>{
-	if(counter >0){
-		setCounter(counter - 1);
-	}	
-  }
+	const removeValue = () => {
+		if (counter > 0) {
+			setCounter(counter - 1);
+		}
+	}
 
-  return (
-	<>
-	<h1>Hiii</h1>
-	<h2> Counter value : {counter}</h2>
+	return (
+		<>
+			<h1>Hiii</h1>
+			<h2> Counter value : {counter}</h2>
 
-	<button onClick={addvalue}> Add Value </button>
+			<button onClick={addvalue}> Add Value </button>
 
-	<br/>
+			<br />
 
-	<button onClick={removeValue}> Remove Value </button>
-	</>
-  )
+			<button onClick={removeValue}> Remove Value </button>
+		</>
+	)
 }
 
 export default App
